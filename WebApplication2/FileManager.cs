3d7906a73e91015fd6a3a902228c34dba9b5a1fe
Thanh Vong <vdthanh91@gmail.com>
2019-12-19 10:22:10 +0700

    new rufa toolkit

---
+++ WebApplication2/FileManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class FileManager
+    {
+        public void OpenFile()
+        {
+            string env = RufaToolkit._ENVIRONMENT;
+
+            RufaToolkit rufaToolkit = new RufaToolkit();
+            string uat = rufaToolkit._UATCONFIG;
+        }
+    }
+}
